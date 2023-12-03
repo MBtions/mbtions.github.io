a386f9f67a9364efd22ce2305fcbf97576cb9581
Meenakshi Bharadwaj <minakshi29bharadwaj@gmail.com>
2023-12-04 00:31:02 +0530

    complete calculator

--- calcScript.js
+++ calcScript.js
@@ -1,28 +1,79 @@
+prev = ""
+exp = ""
+op = ""
 calculation = ""
-let arr = Array.from(document.querySelectorAll("button")).forEach((button)=>{
+input = document.getElementById("result");
+expression = document.getElementById("expStr");
+Array.from(document.querySelectorAll("button")).forEach((button)=>{
     button.addEventListener('click', (e) => {
         console.log(e.target);
-        prev = ""
         if (e.target.innerHTML === "=") {
+            prev = calculation
+            op = e.target.innerHTML
+            calculation = eval(calculation);
             console.log("Res: "+eval(calculation))
+            input.setAttribute("value", calculation);
+            console.log("Op:"+op);
+            console.log("Prev:"+prev);
         }
         else if (e.target.innerHTML === "AC"){
             calculation = ""
-        } else {
+            op = ""
+            exp = ""
+            expression.setAttribute("value", exp);
+            input.setAttribute("value", calculation);
+            prev = calculation
+            console.log("Op:"+op);
+            console.log("Prev:"+prev);
+        }
+        else if (e.target.innerHTML === "C") {
+            if (input.value !== "") {
+                if (op !== "=") {
+                    //calculation = prev
+                    calculation = calculation.slice(0, -1);
+                    exp = exp.slice(0, -1);
+                    input.setAttribute("value", calculation);
+                    expression.setAttribute("value", exp);
+                    if (input.value == "") {
+                        exp = input.value
+                        expression.setAttribute("value", exp);
+                    }
+                } 
+                else {
+                    console.log("Prev:"+prev);
+                    calculation = prev
+                    input.setAttribute("value", calculation);
+                    op = ""
+                }
+            } else {
+                expression.setAttribute("value", "");
+            }
+        }
+        else if (e.target.innerHTML === "%") {
+            calculation += "/100"
+            op = ""
+            exp += e.target.innerHTML
+            expression.setAttribute("value", exp);
+            calculation = eval(calculation)
+            input.setAttribute("value", calculation);
+            prev = calculation
+            console.log("Op:"+op);
+            console.log("Prev:"+prev);
+        }
+        else {
+            prev = calculation
+            op = ""
+            console.log("Op:"+op);
             calculation += e.target.innerHTML
+            exp += e.target.innerHTML
+            expression.setAttribute("value", exp)
+            input.setAttribute("value", calculation);
             console.log("Cal: "+calculation);
+            console.log("Prev:"+prev);
         }
     })
 })
-function calculate(){
-    try{
-        const result = eval(calculation);
-        input.innerHTML = result;
-        calculation='';
-    } catch(e) {
-        alert('Error!');
-    }
-}
+
 /*
 exp = ""
 calculation = ""
